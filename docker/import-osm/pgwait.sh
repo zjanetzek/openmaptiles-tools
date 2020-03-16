195450f036cbb6f021a9a18b79e57540d86785c2
Hannes Janetzek <hannes.janetzek@gmail.com>
2020-03-16 15:43:22 +0100

    wip: disable osml10n

--- docker/import-osm/pgwait.sh
+++ docker/import-osm/pgwait.sh
@@ -14,14 +14,14 @@ export PGDATABASE="${POSTGRES_DB:-${PGDATABASE?}}"
 export PGUSER="${POSTGRES_USER:-${PGUSER?}}"
 export PGPORT="${POSTGRES_PORT:-${PGPORT:-5432}}"
 
-: "${MAX_RETRIES:=40}"  # Maximum number of pg_isready calls
-tries=0
-while ! pg_isready -q
-do
-    tries=$((tries + 1))
-    if (( tries > MAX_RETRIES )); then
-        echo "... gave up waiting for Postgres:   PGHOST=${PGHOST} PGDATABASE=${PGDATABASE} PGUSER=${PGUSER} PGPORT=${PGPORT} pg_isready"
-        exit 1
-    fi
-    sleep 2
-done
+# : "${MAX_RETRIES:=40}"  # Maximum number of pg_isready calls
+# tries=0
+# while ! pg_isready -q
+# do
+#     tries=$((tries + 1))
+#     if (( tries > MAX_RETRIES )); then
+#         echo "... gave up waiting for Postgres:   PGHOST=${PGHOST} PGDATABASE=${PGDATABASE} PGUSER=${PGUSER} PGPORT=${PGPORT} pg_isready"
+#         exit 1
+#     fi
+#     sleep 2
+# done
--- sql/language.sql
+++ sql/language.sql
@@ -69,8 +69,9 @@ CREATE OR REPLACE FUNCTION get_latin_name(tags hstore, geometry geometry) RETURN
         ELSE NULL
       END,
       NULLIF(tags->'name:en', ''),
-      NULLIF(tags->'int_name', ''),
-      NULLIF(osml10n_get_name_without_brackets_from_tags(tags, 'en', geometry), '')
+      NULLIF(tags->'int_name', '')
+      --,
+      --NULLIF(osml10n_get_name_without_brackets_from_tags(tags, 'en', geometry), '')
     );
 $$ LANGUAGE SQL IMMUTABLE STRICT;
 
